f0f35fc1090d2c419bc6a18203f37661107ad01d
lukesmith <stuff@lukesmith.net>
2016-07-21 20:23:37 +0100

    split agents specific api methods out

---
+++ api/agents.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (api *ApiClient) GetAgents() ([]Agent, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", api.ServerUrl+"/agents", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Accept", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	var record []Agent
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		return nil, err
+	}
+
+	return record, nil
+}
